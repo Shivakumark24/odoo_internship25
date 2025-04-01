3738bba2c1264c1b264d1118eb77783c211377ee
Shivakumark24 <shivakumarkhot6@gmail.com>
2025-04-01 22:56:53 +0530

    search view,pivot,kanbann,graph

---
+++ odoo_internship25/sample_module/__manifest__.py
@@ -0,0 +1,25 @@
+
+{
+    'name': 'sample_module',
+    'version': '18.0.0.0',
+    'summary': 'madre mia',
+    'description': """ This Module for training purposes.
+    """,
+    'category':'',
+    'author': 'shivakumar khot',
+    'website': 'www.zbeanztech.com',
+    "license": "LGPL-3",
+    'depends': [],
+    'data': [
+				'security/ir.model.access.csv',
+				'security/security.xml',
+				'views/model_one_view.xml',
+				'views/menu.xml',
+				
+        ],
+    'test': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
